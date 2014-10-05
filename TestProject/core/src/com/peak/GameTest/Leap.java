72e5e8410317f91a7719f77689ad8fd7a854d9e5
herzberd <danielherzberg@Daniels-Mac-mini.local>
2014-10-04 20:51:36 -0500

    Uploaded gradle
    Does not work, will fix in eventual future time thing

--- TestProject/core/src/com/peak/GameTest/Leap.java
+++ TestProject/core/src/com/peak/GameTest/Leap.java
@@ -1,3 +1,5 @@
+package com.peak.GameTest;
+
 import com.leapmotion.leap.*;
 
 public class Leap
--- TestProject/core/src/com/peak/GameTest/LeapListener.java
+++ TestProject/core/src/com/peak/GameTest/LeapListener.java
@@ -1,3 +1,5 @@
+package com.peak.GameTest;
+
 import com.leapmotion.leap.*;
 
 public class LeapListener extends Listener
